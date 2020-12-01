96750b6d2eaa1c860bbbcd8a7b691fd312f92e69
jjfcopy <jjfcopy@debian.jjf>
2020-12-01 14:22:03 +0800

    change user table's character from char into int

--- catfood/user/models.py
+++ catfood/user/models.py
@@ -2,23 +2,16 @@ from django.db import models
 from django import forms
 
 
-class Character(models.TextChoices):
-    TEACHER = 'Teacher', 'Teacher'
-    TA = 'TeachingAssistant', 'TeachingAssistant'
-
 
 class User(models.Model):
     user_id = models.AutoField(primary_key=True)
     password_digest = models.CharField(max_length=50)
     realname = models.CharField(max_length=50)
-    school_id_number = models.CharField(max_length=20)
+    personal_id = models.CharField(max_length=20)
     email = models.EmailField(max_length=50)
     university_id = models.IntegerField()
     school_id = models.IntegerField()
-    character = models.CharField(
-        max_length=32,
-        choices=Character.choices
-    )
+    character = models.IntegerField()
     avatar = models.CharField(max_length=50)
 
 
