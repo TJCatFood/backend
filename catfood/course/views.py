4e58fc8dded4c47c08a28b0e42020381e62097da
Major333 <962461330@qq.com>
2020-12-10 16:04:18 +0800

    bug fix

--- catfood/course/views.py
+++ catfood/course/views.py
@@ -4,7 +4,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.decorators import action
 from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 
 from course.models import Course
 from course.serializers import CourseSerializers
