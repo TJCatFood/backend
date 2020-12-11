af9a97193460c9f8a1ec7fed2f495533deef5a6e
FWdarling <1853204@tongji.edu.cn>
2020-12-11 16:36:59 +0800

    some test contest curd code

--- catfood/contest/urls.py
+++ catfood/contest/urls.py
@@ -5,7 +5,7 @@ from rest_framework import routers
 router = routers.DefaultRouter()
 urlpatterns = [
     path('', views.TestView.as_view()),
-    path('contest/', views.ContestView.as_view()),
+    path('contest', views.ContestView.as_view()),
     path('match/<int:match_id>', views.get_match),
     path('matches/', views.get_matches),
     path('matches/<int:contest_id>', views.MatchesVIew.as_view()),
--- catfood/contest/views.py
+++ catfood/contest/views.py
@@ -5,6 +5,10 @@ from rest_framework.views import APIView
 from rest_framework.decorators import action
 from rest_framework import status
 from rest_framework.decorators import api_view
+from rest_framework.permissions import AllowAny
+
+from contest.serializer import ContestSerializer
+from contest import models
 
 
 @api_view(['GET'])
@@ -27,6 +31,7 @@ def get_match(request, match_id):
 
 
 class TestView(APIView):
+    permission_classes = (AllowAny,)
 
     def get(self, request, format=None):
         response = 'test succeed!'
@@ -37,16 +42,24 @@ class TestView(APIView):
 
 
 class ContestView(APIView):
+    permission_classes = (AllowAny,)
 
     def get(self, request, format=None):
-        return Response('get_contest test succeed')
+        contests = models.Contest.objects.all()
+        serializer = ContestSerializer(contests, many=True)
+        return Response(serializer.data)
 
     def post(self, request, format=None):
         data = request.data
-        return Response(data, status=status.HTTP_201_CREATED)
+        serializer = ContestSerializer(data=data['contest'])
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer._data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status= status.HTTP_400_BAD_REQUEST)
 
 
 class MatchesVIew(APIView):
+    permission_classes = (AllowAny,)
 
     def get(self, request, contest_id, format=None):
         params = request.query_params.dict()
