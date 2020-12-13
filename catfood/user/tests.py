08b467b15af781f85d0879ac61dcb6cfcfc6c4f4
Rika <moreonenightchito@gmail.com>
2020-12-13 20:25:05 +0800

    fix user character

--- catfood/user/tests.py
+++ catfood/user/tests.py
@@ -35,3 +35,18 @@ class UserTests(TestCase):
         self.assertEqual(responseGetAccount.data["data"]["avatar"], "www.baidu.com")
         responseLogout = client.post(testApiPrefix + 'logout/')
         self.assertEqual(responseLogout.status_code, 201)
+
+    def testTeacher(self):
+        client = APIClient()
+        testApiPrefix = '/api/v1/user/'
+        testUserPassword = '123456'
+        responseRegister = client.post(testApiPrefix + 'register/',
+                                       {'password': testUserPassword, 'university_id': self.university_id, 'school_id': self.school_id,
+                                        'realname': 'Rika', 'personal_id': 1888885, "character": 1})
+        self.assertEqual(responseRegister.status_code, 201)        
+        testUserID = responseRegister.data["data"]["user_id"]
+        responseLogin = client.post(testApiPrefix + 'login/',
+                                    {'password': testUserPassword, 'user_id': testUserID})
+        self.assertEqual(responseLogin.status_code, 200)
+        responseGetAccount = client.get(testApiPrefix + 'account/')
+        self.assertEqual(responseGetAccount.data["data"]["character"], '1')
