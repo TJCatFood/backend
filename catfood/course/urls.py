e5916d3d0a061da84e4b1bb5bb4909e172643f39
Major333 <962461330@qq.com>
2021-04-14 16:29:15 +0800

    add api to get students info by course id

--- catfood/course/urls.py
+++ catfood/course/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('course-info/', views.courses_list),
     path('course-info/<int:course_id>', views.course_detail),
     path('teach/', views.teach_list),
-    path('teach/<int:teach_id>', views.teach_detail)
+    path('teach/<int:teach_id>', views.teach_detail),
+    path('course-student-info/<int:course_id>', views.get_students_by_course_id)
 ]
